cfbcd7863f4b4275c3d356b8af12cb3cf337db03
unknown <caoralejayese@gmail.com>
2022-05-25 11:43:30 -0500

    added project terraform

---
+++ src/main/java/com/terraform/SpringbootSimple1Application.java
@@ -0,0 +1,13 @@
+package com.terraform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSimple1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSimple1Application.class, args);
+	}
+
+}
